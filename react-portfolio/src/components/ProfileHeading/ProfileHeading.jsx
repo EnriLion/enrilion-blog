3e0992a31cd66b0996cf12ce4077f6ac2e2d2f7c
EnriLion <enriqueleon0201leon@gmail.com>
2025-08-17 00:34:13 -0600

    Adding files

--- react-portfolio/src/components/ProfileHeading/ProfileHeading.jsx
+++ react-portfolio/src/components/ProfileHeading/ProfileHeading.jsx
@@ -9,11 +9,11 @@ export const ProfileHeading = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Diego Leon</h1>
-            <p classNam={styles.description}>I'm a Jr Developer with 1 year of 
+            <p className={styles.description}>I'm a Jr Developer with +1 year of 
                 experience using Java Spring Boot and 
                 more. Contact me if you are interested!
             </p>
-            <a href="mailto:diegoleonc45@gmail.com" className={styles.contactImg}>Communicate with me!</a>
+            <a href="mailto:diegoleonc45@gmail.com" className={styles.contactBtn}>Communicate with me!</a>
             </div>    
             <img className={styles.profileImg} src={getImageUrl("profile/profile_icon.png")} alt="Portrait of me"/>
             <div className={styles.topBlur}/>
